5d668fd36a7599df344137b5c4094d7303a944c0
Ali <a.k.ezzeddine@gmail.com>
2022-12-01 11:57:02 -0500

    fixed logged out bug issues

--- react-app/src/components/AccountPage/AccountPage.js
+++ react-app/src/components/AccountPage/AccountPage.js
@@ -24,8 +24,10 @@ const AccountPage = () => {
             setUser(user)
         })();
         (async () => {
-            const followData = await dispatch(followActions.userFollowList(sessionUser.id))
-            setCurrentUserFollowers(followData)
+            if (sessionUser) {
+                const followData = await dispatch(followActions.userFollowList(sessionUser.id))
+                setCurrentUserFollowers(followData)
+            }
         })();
         (async () => {
             const followData = await dispatch(followActions.userFollowList(userId))
@@ -71,7 +73,7 @@ const AccountPage = () => {
         }
     } else {
         followButton = (
-            <button hidden={sessionUser.id === Number(userId)} onClick={() => history.push("/login")}>Follow</button>
+            <button onClick={() => history.push("/login")}>Follow</button>
         )
     }
     const follow = (e) => {
@@ -86,69 +88,118 @@ const AccountPage = () => {
         dispatch(followActions.unfollowUser(sessionUser.id, user.id))
         console.log("unfollow")
     }
-    if (sessionUser.id === Number(userId)) {
-        return (
-            <div className="profile-container">
-                <div className="profile-header-container" style={{ display: "flex", flexDirection: "row" }}>
-                    <div className="user-profile-pic">
-                        {profilePic}
+    if (sessionUser) {
+        if (sessionUser.id === Number(userId)) {
+            return (
+                <div className="profile-container">
+                    <div className="profile-header-container" style={{ display: "flex", flexDirection: "row" }}>
+                        <div className="user-profile-pic">
+                            {profilePic}
+                        </div>
+                        <div className="user-info">
+                            Profile
+                            <h1 className="user-name" style={{ color: "white" }}>
+                                {sessionUser.username}
+                            </h1>
+                            {userPlaylistLength}
+                            &nbsp;
+                            <span style={{ fontSize: "20px" }}>·</span>
+                            &nbsp;
+                            {currentUserFollowers?.followed_by_user_ids?.length} Followers
+                            &nbsp;
+                            <span style={{ fontSize: "20px" }}>·</span>
+                            &nbsp;
+                            {currentUserFollowers?.current_followed_user_ids?.length} Following
+                        </div>
                     </div>
-                    <div className="user-info">
-                        Profile
-                        <h1 className="user-name" style={{ color: "white" }}>
-                            {sessionUser.username}
-                        </h1>
-                        {userPlaylistLength}
-                        &nbsp;
-                        <span style={{ fontSize: "20px" }}>·</span>
-                        &nbsp;
-                        {currentUserFollowers?.followed_by_user_ids?.length} Followers
-                        &nbsp;
-                        <span style={{ fontSize: "20px" }}>·</span>
-                        &nbsp;
-                        {currentUserFollowers?.current_followed_user_ids?.length} Following
+                    <div className="follow-user-container">
+                        {followButton}
+                    </div>
+                    <div className="profile-content">
+                        <h2>Playlists</h2>
+                        <div className="profile-playlists-container">
+                            {userPlaylistList && (
+                                userPlaylistList.map((playlist) => {
+                                    return <div>
+                                        <div className="playlist-image">
+                                            <Link to={`/playlist/${playlist.id}`}>
+                                                <img className="playlist-image-profile" src={playlist.playlist_img} />
+                                            </Link>
+                                        </div>
+                                        <div className="playlist-name">
+                                            {playlist.name}
+                                        </div>
+                                    </div>
+                                })
+                            )}
+                        </div>
+                        <h2>Followed Playlists</h2>
+                        <div className="profile-followed-playlists-container">
+                            {followingPlaylists && (
+                                followingPlaylists.map((playlist) => {
+                                    return <div>
+                                        <div className="playlist-image">
+                                            <Link to={`/playlist/${playlist.id}`}>
+                                                <img className="playlist-image-profile" src={playlist.playlist_img} />
+                                            </Link>
+                                        </div>
+                                        <div className="playlist-name">
+                                            {playlist.name}
+                                        </div>
+                                    </div>
+                                })
+                            )}
+                        </div>
+                    </div>
+                </div>)
+        } else {
+            return (
+                <div className="profile-container">
+                    <div className="profile-header-container" style={{ display: "flex", flexDirection: "row" }}>
+                        <div className="user-profile-pic">
+                            {profilePic}
+                        </div>
+                        <div className="user-info">
+                            Profile
+                            <h1 className="user-name" style={{ color: "white" }}>
+                                {user.username}
+                            </h1>
+                            {userPlaylistLength}
+                            &nbsp;
+                            <span style={{ fontSize: "20px" }}>·</span>
+                            &nbsp;
+                            {profileFollowers?.followed_by_user_ids?.length} Followers
+                            &nbsp;
+                            <span style={{ fontSize: "20px" }}>·</span>
+                            &nbsp;
+                            {profileFollowers?.current_followed_user_ids?.length} Following
+                        </div>
+                    </div>
+                    <div className="follow-user-container">
+                        {followButton}
+                    </div>
+                    <div className="profile-content">
+                        <h2>Playlists</h2>
+                        <div className="profile-playlists-container">
+                            {userPlaylistList && (
+                                userPlaylistList.map((playlist) => {
+                                    return <div>
+                                        <div className="playlist-image">
+                                            <Link to={`/playlist/${playlist.id}`}>
+                                                <img className="playlist-image-profile" src={playlist.playlist_img} />
+                                            </Link>
+                                        </div>
+                                        <div className="playlist-name">
+                                            {playlist.name}
+                                        </div>
+                                    </div>
+                                })
+                            )}
+                        </div>
                     </div>
                 </div>
-                <div className="follow-user-container">
-                    {followButton}
-                </div>
-                <div className="profile-content">
-                    <h2>Playlists</h2>
-                    <div className="profile-playlists-container">
-                        {userPlaylistList && (
-                            userPlaylistList.map((playlist) => {
-                                return <div>
-                                    <div className="playlist-image">
-                                        <Link to={`/playlist/${playlist.id}`}>
-                                            <img className="playlist-image-profile" src={playlist.playlist_img} />
-                                        </Link>
-                                    </div>
-                                    <div className="playlist-name">
-                                        {playlist.name}
-                                    </div>
-                                </div>
-                            })
-                        )}
-                    </div>
-                    <h2>Followed Playlists</h2>
-                    <div className="profile-followed-playlists-container">
-                        {followingPlaylists && (
-                            followingPlaylists.map((playlist) => {
-                                return <div>
-                                    <div className="playlist-image">
-                                        <Link to={`/playlist/${playlist.id}`}>
-                                            <img className="playlist-image-profile" src={playlist.playlist_img} />
-                                        </Link>
-                                    </div>
-                                    <div className="playlist-name">
-                                        {playlist.name}
-                                    </div>
-                                </div>
-                            })
-                        )}
-                    </div>
-                </div>
-            </div>)
+            )
+        }
     } else {
         return (
             <div className="profile-container">
