736b54ca005f68dab83e8ac2c62fd51605f1759e
Ali Ezzeddine <105993056+alkezz@users.noreply.github.com>
2022-12-04 20:38:21 -0500

    Update 34b500648488_.py

--- migrations/versions/34b500648488_.py
+++ migrations/versions/34b500648488_.py
@@ -8,6 +8,10 @@ Create Date: 2022-11-23 12:02:45.856708
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 # revision identifiers, used by Alembic.
 revision = '34b500648488'
 down_revision = None
