1e5119bf292c6b1f4c6657c55fdec4cd8d9e79e9
Ali <a.k.ezzeddine@gmail.com>
2023-03-02 14:14:22 -0500

    added hover effect to modal open to show user it's interactable

--- react-app/src/components/ArtistAboutModal/ArtistAboutModal.js
+++ react-app/src/components/ArtistAboutModal/ArtistAboutModal.js
@@ -4,6 +4,7 @@ import { useParams, Link, useHistory } from "react-router-dom";
 import Modal from '@mui/material/Modal';
 import Grow from '@mui/material/Grow';
 import Backdrop from '@mui/material/Backdrop';
+import "./ArtistAboutModal.css"
 
 const ArtistAboutModal = ({ artistBio, artistImg }) => {
     const [open, setOpen] = useState(false)
@@ -12,7 +13,7 @@ const ArtistAboutModal = ({ artistBio, artistImg }) => {
     return (
         <div className='artist-biography-container' style={{ marginLeft: "40px" }}>
             <h2 style={{ color: "white" }}>About</h2>
-            <div onClick={handleOpen} className='artist-image-bio' style={{ borderRadius: "10px", backgroundSize: "1000px 550px", display: "flex", alignItems: "flex-end", color: "white", marginRight: "500px", fontSize: "18px", cursor: "pointer", backgroundImage: `url(${artistImg})`, height: "550px", backgroundRepeat: "no-repeat" }}>
+            <div onClick={handleOpen} className="growImage" style={{ borderRadius: "10px", backgroundSize: "1000px 550px", display: "flex", alignItems: "flex-end", color: "white", marginRight: "500px", fontSize: "18px", cursor: "pointer", backgroundImage: `url(${artistImg})`, height: "550px", backgroundRepeat: "no-repeat" }}>
                 <div style={{ width: "75%", marginBottom: "50px", marginLeft: "60px", textShadow: "2px 2px 2px rgba(0, 0, 0, 1)" }}>{artistBio?.slice(0, 208)}...</div>
             </div>
             <Modal open={open}
@@ -23,7 +24,7 @@ const ArtistAboutModal = ({ artistBio, artistImg }) => {
             >
                 <Grow timeout={500} in={open} style={{ transformOrigin: "bottom center" }}>
                     <div style={{ width: "50%", marginLeft: "30%", marginTop: "10%" }}>
-                        <div style={{ backgroundColor: "#121212", padding: "15px" }}>
+                        <div style={{ backgroundColor: "#121212", padding: "15px", borderRadius: "10px" }}>
                             <img src={artistImg} style={{ width: "100%", height: "400px" }} />
                             <span style={{ color: "white" }}>{artistBio}</span>
                         </div>
